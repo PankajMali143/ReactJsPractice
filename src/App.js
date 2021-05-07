bf17799aa461dcd33aaff44c87389fced0b7e1e0
Pankaj Popat Mali <68640975+PankajMali143@users.noreply.github.com>
2021-05-07 19:16:34 +0530

    user add function added adeed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import "./styles.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <button id="user-btn">Add User</button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const api = `https://randomuser.me/api`;
+console.log(api.length);
+
 const addUser = document.getElementById("user-btn");
+const Mydiv = document.getElementById("app");
 
 addUser.addEventListener("click", async () => {
   const userData = await fetch(api, {
@@ -7,5 +10,13 @@ addUser.addEventListener("click", async () => {
   });
 
   const userJson = await userData.json();
-  console.log(userJson.results[0]);
+  const user = userJson.results[0];
+  console.log(user);
+
+  const userEle = document.createElement("div");
+  userEle.innerHTML = `<div>
+  ${user.name.title} ${user.name.first} ${user.name.last}
+  </div>`;
+
+  Mydiv.appendChild(userEle);
 });
