e61f297e0caac70ed6fdf4e634ebc08aa2d26ba2
Pankaj Popat Mali <68640975+PankajMali143@users.noreply.github.com>
2021-05-15 15:33:03 +0530

    class used in js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,20 @@ const api = `https://randomuser.me/api`;
 const addUser = document.getElementById("user-btn");
 const newdiv = document.getElementById("user-list");
 const search = document.getElementById("search");
+const sortdesc = document.getElementById("sort-desc");
+const sortasc = document.getElementById("sort-asc");
 
 //used empty array
 const appState = [];
+
+class User {
+  constructor(title, firstname, lastname, gender, email) {
+    this.name = `${title} ${firstname} ${lastname}`;
+    this.emil = email;
+    this.gender = gender;
+  }
+}
+
 //added onclick event listener to get api data in another variable
 addUser.addEventListener("click", async () => {
   const userData = await fetch(api, {
@@ -20,7 +31,15 @@ addUser.addEventListener("click", async () => {
   //taking array data in user variable
   const user = userJson.results[0];
   //now pushing only rquired data in appstate array that we have declared
-  appState.push(user);
+  const classUser = new User(
+    user.name.title,
+    user.name.first,
+    user.name.last,
+    user.gender,
+    user.email
+  );
+  console.log(classUser);
+  appState.push(classUser);
   console.log("data in appstate array is =>", appState);
   //we are calling domRender funtion & passing appstate array
   domRender(appState);
@@ -35,8 +54,14 @@ const domRender = (stateArr) => {
     const userEle = document.createElement("div");
 
     userEle.innerHTML = `<div>
-  ${userObj.name.title} ${userObj.name.first} ${userObj.name.last}
+  Name :${userObj.name}
+
+  <ol>
+    <li>${userObj.gender}</li>
+    <li>${userObj.email}</li>
+    </ol>
   </div>`;
+
     //and finally we are appending data to div
     newdiv.appendChild(userEle);
   });
@@ -44,8 +69,23 @@ const domRender = (stateArr) => {
 
 search.addEventListener("keyup", (e) => {
   console.log(e, search.value);
-  const fillterAppstate = appState.filter((user) =>
-    user.name.first.toLowerCase().includes(search.value.toLowerCase())
+  const fillterAppstate = appState.filter(
+    (user) =>
+      user.name.toLowerCase().includes(search.value.toLowerCase()) ||
+      user.gender.toLowerCase().includes(search.value.toLowerCase()) ||
+      user.email.toLowerCase().includes(search.value.toLowerCase())
   );
   domRender(fillterAppstate);
+});
+
+sortdesc.addEventListener("click", () => {
+  const stateArrCopy = [...appState];
+  stateArrCopy.sort((a, b) => (a.name < b.name ? 1 : -1));
+  domRender(stateArrCopy);
+});
+
+sortasc.addEventListener("click", () => {
+  const stateArrCopy = [...appState];
+  stateArrCopy.sort((a, b) => (a.name > b.name ? 1 : -1));
+  domRender(stateArrCopy);
 });
